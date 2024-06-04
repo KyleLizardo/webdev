5b2c4234fa6de0a3d3546f03a7100891611ffbab
Kyle Lizardo <kyleethan442@gmail.com>
2024-06-05 02:33:22 +0800

    dfd

--- MainPage/assist.js
+++ MainPage/assist.js
@@ -1,7 +1,12 @@
+
+
 // Show the title input when the textarea is focused
 document.getElementById('request-textarea').addEventListener('focus', () => {
     document.getElementById('title-input').style.display = 'block'; // Removed space between 9 and rem
     document.getElementById('close').style.display = 'block';
+
+
+    this.style.overflow = 'hidden'; 
 
 });
 
@@ -12,7 +17,10 @@ document.getElementById('close').addEventListener('click', () => {
     titleInput.style.display = titleInput.style.display === 'none' ? 'block' : 'none';
     document.getElementById('request-textarea').style.height = '6rem';
     document.getElementById('close').style.display = 'none';
+    
     document.getElementById('file').style.marginBottom = '35px';
+
+  
 
 });
 
@@ -27,8 +35,8 @@ requestTextarea.addEventListener('input', function () {
     this.style.height = `${scrollHeight}px`; // Set height based on content
   
     // Enable overflow only when scrollHeight reaches or exceeds 700px
-    if (scrollHeight >= 700) {
-      this.style.maxHeight = '700px'; // Set max height for restriction
+    if (scrollHeight >= 500) {
+      this.style.maxHeight = '500px'; // Set max height for restriction
       this.style.overflow = 'visible'; // Enable overflow
     } else {
       // Remove max height and overflow if content shrinks below 700px
